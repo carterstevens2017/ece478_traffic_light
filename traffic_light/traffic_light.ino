cbf5422649f42a352fab34cf6a74520ac28e5f36
erictuyishime <erictuyishime@gmail.com>
2016-12-01 17:27:31 -0500

    Added random number function

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -1,11 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-
+  // Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
 
 double defuzzify(double dot[6]){
@@ -20,25 +18,35 @@ double defuzzify(double dot[6]){
        z++;
     }
   }
-  finalweights[0] = max(statetable[3], statetable[6], 0.0, 0.0);
-  finalweights[1] = max(statetable[0],statetable[4],statetable[7],statetable[8]);
-  finalweights[2] = max(statetable[1],statetable[5],0.0,0.0);
+  finalweights[0] = maxval(statetable[3], statetable[6], 0.0, 0.0);
+  finalweights[1] = maxval(statetable[0],statetable[4],statetable[7],statetable[8]);
+  finalweights[2] = maxval(statetable[1],statetable[5],0.0,0.0);
   finalweights[3] = statetable[2];
   crisptime = (5.0 * finalweights[0]) + (7.0 * finalweights[1]) + (12.0 * finalweights[2]) + (15.0 * finalweights[3]);
   crisptime = crisptime / (finalweights[0] + finalweights[1] + finalweights[2] + finalweights[3]);
   return crisptime;
 }
 
-double min(double x, double y){
+double minval(double x, double y){
   if(x < y) return x;
   else return y;
 }
 
-double max(double raphael, double michaelangelo, double leonardo, double donatello){
+double maxval(double raphael, double michaelangelo, double leonardo, double donatello){
   if((raphael > michaelangelo) && (raphael > leonardo) && (raphael > donatello)) return raphael;
   if((michaelangelo > raphael) && (michaelangelo > leonardo) && (michaelangelo > donatello)) return michaelangelo;
   if((leonardo > raphael) && (leonardo > michaelangelo) && (leonardo > donatello)) return leonardo;
   if((donatello > raphael) && (donatello > michaelangelo) && (donatello > leonardo)) return donatello;
   return 0.0;
 }
+
+int randNumber(int *array){
+randomSeed(analogRead(0));
+array[0] = random(100);
+array[1] = random(100);
+//return array;
+return 1;
+}
+
+
 
